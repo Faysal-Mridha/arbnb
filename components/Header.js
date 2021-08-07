c86a5a12ae03abc1e9865e89e4d904d44a97b95c
Faysal-Mridha <fmridha166@gmail.com>
2021-08-06 20:37:40 -0700

    day two

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,59 @@
 import Image from 'next/image'
 import { SearchIcon,GlobeAltIcon,UserCircleIcon,MenuIcon,UsersIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+import {useRouter} from 'next/router'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
-function Header() {
+
+function Header({placeholder}) {
+    const [input, setInput] = useState('');
+    const [startDate,setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [numOfGuests,setNumOfGuests] =useState(1)
+
+    const router = useRouter();
+
+    const selectionRange = {
+      startDate: startDate,
+      endDate: endDate,
+      key: 'selection'
+    }
+
+    const handleSelect = (ranges) => {
+       setStartDate(ranges.selection.startDate)
+       setEndDate(ranges.selection.endDate)
+    }
+
+    const handleClick = () => {
+        // router.push({
+        //     pathname: "/search",
+        //     query: {
+        //         location: input,
+        //         startDate: startDate.toISOString(),
+        //         endDate: endDate.toISOString(),
+        //         numOfGuests,
+        //     }
+        // });
+
+        // TODO another way
+
+        router.push(`/search?location=${input}&startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&numOfGust=${numOfGuests}`)
+    }
+
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/* Left */}
             <div className='relative flex items-center h-10 cursor-pointer my-auto animate-bounce'>
-                <Image src="https://links.papareact.com/qd3" layout='fill' objectFit='contain' objectPosition='left' />
-                {/* <Image src="/img/logo.png" layout='fill' objectFit='contain' objectPosition='left' /> */}
+                {/* <Image src="https://links.papareact.com/qd3" layout='fill' objectFit='contain' objectPosition='left' /> */}
+
+                <Image onClick={()=> router.push('/')} src="/img/logo.png" layout='fill' objectFit='contain' objectPosition='left' />
             </div>
             {/* middle */}
             <div className="flex items-center border-2 rounded-full py-2">
-                <input className='outline-none pl-5 bg-transparent flex-grow text-gray-600 placeholder-gray-700' type="text" placeholder='Start your Search.' />
+                <input value={input} onChange={(e)=> setInput(e.target.value)} className='outline-none pl-5 bg-transparent flex-grow text-gray-600 placeholder-gray-700' type="text" placeholder={placeholder || 'Start your Search.'} />
 
                 <SearchIcon className='hidden md:inline-flex bg-red-400 text-white rounded-full p-2 cursor-pointer h-8 md:mx-2' />
             </div>
@@ -24,6 +66,33 @@ function Header() {
                     <UserCircleIcon className='h-6' />
                 </div>
             </div>
+
+            {input && (
+                <div className='flex flex-col col-span-3 mx-auto'>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        minDate={new Date()}
+                        rangeColors={['#FD5861']}
+                    />
+
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+
+                        <UsersIcon className="h-5" />
+
+                        <input type="number" value={numOfGuests} onChange={(e) => setNumOfGuests(e.target.value)} max={99} min={1} className="w-12 pl-2 text-lg outline-none text-red-400"/>
+
+                    </div>
+
+                    <div className='flex items-center justify-around'>
+                        <button onClick={()=> setInput('')} className="text-gray-500 focus:outline-none">Cancel</button>
+                        <button onClick={handleClick} className="text-red-400 font-semibold focus:outline-none">Search</button>
+                    </div>
+
+                </div>
+            )}
+
         </header>
     )
 }
---
+++ pages/search.js
@@ -0,0 +1,58 @@
+import { format } from "date-fns";
+import { useRouter } from "next/router";
+import Fotter from "../components/Fotter";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
+
+function Search({searchReasult}) {
+    const router = useRouter();
+    
+    const { startDate, numOfGust, location, endDate } = router.query;
+    
+    const formatedStartDate = format(new Date(startDate), "dd MMMM yy");
+    const formatedEndDate = format(new Date(endDate), "dd MMMM yy");
+    const range = `${formatedStartDate} - ${formatedEndDate}`;
+    
+    return (
+        <div>
+            {/* Header */}
+            <Header placeholder={`${location} | ${range} | ${numOfGust}`} />
+            {/* main */}
+            <main className='flex max-w-[500px]'>
+                <section className='flex-grow pt-14 px-6'>
+                    <p className='text-sm'>300+ Stays - {range} - for {numOfGust} guests</p>
+
+                    <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
+
+                    <div className='mb-5 hidden lg:inline-flex flex space-x-4 whitespace-nowrap'>
+                        <p className='btn'>Cancellation Flexibility</p>
+                        <p className='btn'>Type of Place</p>
+                        <p className='btn'>Price</p>
+                        <p className='btn'>Rooms and Beds</p>
+                        <p className='btn'>More fillters</p>
+                    </div>
+
+                        {searchReasult?.map(({ img, title, location, description, star, price, text,total }) => (
+                            <InfoCard key={img} img={img} title={title} location={location} description={description} star={star} price={price} text={text} total={total}/>
+                        ))}
+
+                </section>
+            </main>
+            {/* fotter */}
+            <Fotter />
+        </div>
+    )
+}
+
+export default Search
+
+export async function getServerSideProps(context) {
+    const searchReasult = await fetch('https://links.papareact.com/isz').then((response) => response.json())
+
+    return {
+        props: {
+            searchReasult
+        }
+    }
+    
+}
